43409a3f3e8ea48fb115906c93a60110b838d645
Unknown-user207 <mohammedosman4013@gmail.com>
2024-12-03 03:59:45 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "GHOST-MD&6Lip9Cc1",
+  SESSION_ID: process.env.SESSION_ID || "GHOST-MD&FNa27mCX",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
